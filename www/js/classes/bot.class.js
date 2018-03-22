6a31c04ad34addbc76c0d430771168653661cc30
Unknown <urban.eksvard@gmail.com>
2018-03-22 10:59:49 +0100

    pushed bot code  and class

---
+++ www/js/classes/bot.class.js
@@ -0,0 +1,12 @@
+class Bot extends Player{
+
+	constructor(name, type, color) {
+		super(name, type, color);
+		this.difficulty = 'easy';
+		console.log('my name is:', this.name)
+	}
+
+	botMove() {
+		board.findLastEmptyCell(col) 
+	}
+}
