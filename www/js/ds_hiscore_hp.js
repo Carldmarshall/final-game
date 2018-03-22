50bd05db9afe5d540c914180e3dedb8168ba0909
sandhya <ksandhya22@gmail.com>
2018-03-22 15:19:29 +0100

    Changes has done at
    ds_hiscore_hp.js

--- www/js/ds_hiscore_hp.js
+++ www/js/ds_hiscore_hp.js
@@ -1,30 +1,12 @@
+$(function (){
+  JSON._load("hiscore.json").then(function(data) {
+    //SCORE IN DECENDING ORDER
+    data = data.sort(function(a,b){return b.Score - a.Score});
 
-     
-        $(document).ready(function ()
-        {
-        $.getJSON("json/hiscore.json", function (data) 
-        {
-
-           let arrItems = [];      // THE ARRAY TO STORE JSON ITEMS.
-
-           data = data.sort(function(a,b){return b.Score - a.Score}); //SCORE IN DECENDING ORDER
-
-            $.each(data, function (index, value)
-             {
-                arrItems.push(value);       // PUSH THE VALUES INSIDE THE ARRAY.
-             });
-
-          //document.getElementById("dis1").innerHTML = JSON.stringify(arrItems[0]);
-			$('#display').html(arrItems[0].Name + " " + " " + " " +arrItems[0].Score);
-
-          
-          
-
-             
-
-
-        });
-    }); 
+    // display first
+    $('#display').html(data[0].Name + "   " +data[0].Score);
+  });
+}); 
 
 
    
