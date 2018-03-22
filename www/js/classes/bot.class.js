fe4bec7c13474b007792a9e7c7756cd31c5e4431
Unknown <urban.eksvard@gmail.com>
2018-03-22 14:17:30 +0100

    game images
    bot code

--- www/js/classes/bot.class.js
+++ www/js/classes/bot.class.js
@@ -10,3 +10,40 @@ class Bot extends Player{
 	//	board.findLastEmptyCell(col) 
 	}
 }
+
+
+/* Dennis board codefunction click randomColumn...
+	1. make a random Number */
+
+	let botChoice = Math.floor(Math.random());
+//	2. make a random column click
+	$('.col' + botChoice).first().click();
+
+
+/*function tryUntilValidClick
+1. let isDone = false;
+2. while (isDone == false){
+	doThaClick
+	if(playerTurnChanged{
+	isDone = true;
+	})
+}
+
+function simulateBotTURN
+	set Timeout(function(){
+	tryUntilValidClick;
+
+	},2000);
+
+	if(gameISOver){
+	
+	}
+
+	makeTurn(){
+	if (currentPlayer.isBot){
+		simulateBotTurn();
+	}
+	if(gameIsOver){
+	quit
+	};
+	}
