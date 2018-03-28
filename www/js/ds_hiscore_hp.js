45de20b7a2fd1b42a04419dcc4b2ff9277bdd9ba
Unknown <carl.daniel.marshall@gmail.com>
2018-03-28 13:54:29 +0200

    New High Score display.

--- www/js/ds_hiscore_hp.js
+++ www/js/ds_hiscore_hp.js
@@ -4,26 +4,6 @@ $(function (){
     data = data.sort(function(a,b){return a.score - b.score});
 
     // display first
-    $('#display').html(data[0].name + "<br/>" +data[0].score);
+    $('#display').html(('Bestplayer') +" " + ":"  + " " + data[0].name + " " +data[0].score);
   });
-}); 
-
-
-   
-
-                    
-                    
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
+});
